1ac3d6d0ccbd5c7fd6210ae35458da52a3a023f7
Agrainel <agrainel@list.ru>
2026-06-20 22:04:32 +0300

    Корректировка файла main.py (запуск: python main.py)

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env puthon3
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 print('Hello from repository!')
+
+def print_author():
+    author = os.getenv("AUTHOR")
+    return author
+
+print(f"Автор проекта: {print_author()}")
